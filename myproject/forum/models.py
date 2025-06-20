3fb5dd575278512a69f65ba18a237a67cda4f467
Olya Milch <323234214213m@gmail.com>
2025-06-20 18:22:37 +0300

    Create User model and filling media-settings in settings.py

--- myproject/forum/models.py
+++ myproject/forum/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# My models here.
+
+class User(models.Model):
+    nickname = models.CharField(max_length=40, unique=True)  # Be sure to specify the field type in objects in Django ('models.CharField' - or other)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)  # The folder is created automatically on first boot.
+    sex_choice = [
+        ('Ж', 'Женский'),
+        ('М', 'Мужской'),
+    ]
+    sex = models.CharField(max_length=1, choices=sex_choice)
+
+    def __str__(self):
+        return self.nickname
+
