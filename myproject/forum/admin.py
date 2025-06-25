3ac857018d7d52f9247e0bbed223a243b58fcb2f
Olya Milch <323234214213m@gmail.com>
2025-06-25 12:09:40 +0300

    Registering models in admin.py

--- myproject/forum/admin.py
+++ myproject/forum/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Post, Comment
 
-# Register your models here.
+
+# Register my models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
+
+
