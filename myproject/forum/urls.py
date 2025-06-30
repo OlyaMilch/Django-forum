03c3dcaa5ebef61f17212bbb4df3e781b966122e
Olya Milch <323234214213m@gmail.com>
2025-06-30 14:22:55 +0300

    Create roots in forum\urls.py

---
+++ myproject/forum/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserView, PostView, CommentView, LikeView
+
+
+router = DefaultRouter()  # Create a router that automatically creates URL routes
+router.register(r'users', UserView)  # Creates all available routes using UserView
+router.register(r'posts', PostView)
+router.register(r'comments', CommentView)
+router.register(r'likes', LikeView)
+
+"""
+here separate list of urlpatterns
+myproject/urls.py — "master route distributor"
+forum/urls.py — routes for the API forum only
+"""
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
