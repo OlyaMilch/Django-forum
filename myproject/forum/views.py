ca4f888193fc8c4b30144814bd94d11d937923b5
Olya Milch <323234214213m@gmail.com>
2025-07-05 12:23:34 +0300

    Add access rights in views.py

--- myproject/forum/views.py
+++ myproject/forum/views.py
@@ -1,13 +1,15 @@
 from rest_framework import viewsets
 from .models import User, Post, Comment, Like
 from .serializers import UserSerializer, PostSerializer, CommentSerializer, LikeSerializer
+from .permissions import ReadOnly, AdminAndOwner
 
 
 
 '''
-Views.py answers what to do with requests, how to work with them
+views.py is the glue between the models and the client. It controls what exactly happens when the user makes an HTTP request.
 Here I use data in JSON and not in HTML
 '''
+
 
 
 class UserView(viewsets.ModelViewSet):  # viewsets.ModelViewSet - This is a class from DRF that automatically creates a full set of actions (CRUD)
@@ -17,10 +19,19 @@ class UserView(viewsets.ModelViewSet):  # viewsets.ModelViewSet - This is a clas
 class PostView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [ReadOnly | AdminAndOwner]  # Restrict access rights to actions on objects
+
+    def perform_create(self, serializer):  # Called automatically when the user sends a POST request
+        serializer.save(author=self.request.user)
 
 class CommentView(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [ReadOnly | AdminAndOwner]  # Author or admin
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 
 class LikeView(viewsets.ModelViewSet):
     queryset = Like.objects.all()
