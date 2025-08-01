f120beb17cb23101c7f6af3da81687061af03892
Olya Milch <323234214213m@gmail.com>
2025-08-01 12:37:17 +0300

    Add post-detail.html also add forum navigation buttons

--- myproject/forum/web_urls.py
+++ myproject/forum/web_urls.py
@@ -9,10 +9,10 @@ otherwise there will be confusion in urls.py
 
 
 urlpatterns = [
-    path('', post_list_view, name='post_list'),
+    path('', post_list_view, name='post_list'),  # List of all posts
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('profile/', profile_view, name='profile'),
     path('logout/', logout_view, name='logout'),
-    path('post/<int:pk>/', post_detail_view, name='post_detail'),
+    path('post/<int:pk>/', post_detail_view, name='post_detail'), # One post with details
 ]
